2e0751773984afaf29a428da16f48e66bd868364
LorgOn <37798530+Lorg0n@users.noreply.github.com>
2022-12-09 00:38:10 +0300

    Update player.js

--- player.js
+++ player.js
@@ -1,73 +1,90 @@
 let tg = window.Telegram.WebApp;
 
 var urlParams = new URLSearchParams(window.location.search);
+var data = JSON.parse(urlParams.get("data"))
 var video = document.getElementById('video');
+var videoName = document.getElementById('video_name')
+
+var prevDiv = document.getElementById('div_prev');
+var nextDiv = document.getElementById('div_next');
+
+var currentVideo = 0;
 
 function playM3u8(url){
-  if(Hls.isSupported()) {
-      video.volume = 0.3;
-      var hls = new Hls();
-      var m3u8Url = decodeURIComponent(url)
-      hls.loadSource(m3u8Url);
-      hls.attachMedia(video);
-      hls.on(Hls.Events.MANIFEST_PARSED,function() {
-        video.play();
-      });
-      document.title = url
-    }
-	else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-		video.src = url;
-		video.addEventListener('canplay',function() {
-		  video.play();
-		});
-		video.volume = 0.3;
-		document.title = url;
-  	}
-}
-
-function playPause() {
-    video.paused?video.play():video.pause();
-}
-
-function volumeUp() {
-    if(video.volume <= 0.9) video.volume+=0.1;
-}
-
-function volumeDown() {
-    if(video.volume >= 0.1) video.volume-=0.1;
-}
-
-function seekRight() {
-    video.currentTime+=5;
-}
-
-function seekLeft() {
-    video.currentTime-=5;
-}
-
-function vidFullscreen() {
-    if (video.requestFullscreen) {
-      video.requestFullscreen();
-  } else if (video.mozRequestFullScreen) {
-      video.mozRequestFullScreen();
-  } else if (video.webkitRequestFullscreen) {
-      video.webkitRequestFullscreen();
+    if(Hls.isSupported()) {
+        video.volume = 0.3;
+        var hls = new Hls();
+        var m3u8Url = decodeURIComponent(url)
+        hls.loadSource(m3u8Url);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED,function() {
+          video.play();
+        });
+        document.title = url
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = url;
+        video.addEventListener('canplay',function() {
+           video.play();
+        });
+        video.volume = 0.3;
+        document.title = url;
     }
 }
 
-nextPlay = () => {
-    console.log('[!] Open next link!')
-    window.open(urlParams.get('next_link'),"_self")
+function nextVideo(){
+    playM3u8(data["episodes"][currentVideo + 1]['url'])
+    currentVideo += 1
 }
 
+function prevVideo(){
+    playM3u8(data["episodes"][currentVideo - 1]['url'])
+    currentVideo -= 1
+}
+
+function showControls(state){
+    stateString = state ? 'visible' : 'hidden'
+    if (state) {
+        if (currentVideo > 0) {
+            prevDiv.style.visibility = stateString
+        } else if (currentVideo < data['episodes'].length) {
+            nextDiv.style.visibility = stateString
+        }
+    } else {
+        prevDiv.style.visibility = stateString
+        nextDiv.style.visibility = stateString
+    }
+}
+
+video.addEventListener('pause', function(e){
+    showControls(true)
+})
+
+video.addEventListener('play', function(e){
+    showControls(false)
+})
 
 Telegram.WebApp.onEvent('viewportChanged', function(e){
     video.style.width = tg.viewportHeight;
 })
 
-playM3u8(urlParams.get('video'))
-$(window).on('load', function () {
-    $('#video').on('click', function(){this.paused?this.play():this.pause();});
-    $('#video').attr('poster', urlParams.get('poster'))
+prevDiv.addEventListener('click', function (event) {
+    prevVideo()
 });
-video.addEventListener('ended', nextPlay, false)
+
+nextDiv.addEventListener('click', function (event) {
+    nextVideo()
+});
+
+video.addEventListener('timeupdate', function(e){
+    videoName.textContent = (data['first'] + currentVideo) + " эпизод"
+})
+
+video.addEventListener('ended', function(e){
+    if (data['autoplay'] && currentVideo < data['episodes'].length){
+        nextVideo()
+    }
+})
+
+$(window).on('load', function () {
+    playM3u8(data['episodes'][0]['url'])
+});
